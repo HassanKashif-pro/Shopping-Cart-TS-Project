9e6d9dfc07b7939f1dbd70b9363cd1e607c6a57f
Hassan Kashif. pro <hassan.kashif.pro@Gmail.com>
2024-05-26 14:47:42 -0700

    Added the currency formatter for the prices in store.

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,4 +1,5 @@
-import { Card } from "react-bootstrap";
+import { Card, Button } from "react-bootstrap";
+import { formatCurrency } from "../utils/formatCurrency";
 
 type StoreItemProps = {
   id: number;
@@ -9,15 +10,23 @@ type StoreItemProps = {
 
 export function StoreItem({ id, imgUrl, name, price }: StoreItemProps) {
   return (
-    <Card>
+    <Card className="h-100">
       <Card.Img
         variant="top"
         src={imgUrl}
         height="200px"
         style={{ objectFit: "cover" }}
       />
+      <Card.Body className="d-flex flex-column">
+        <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
+          <span className="fs-2">{name}</span>
+          <span className="ms-2 text-muted">{formatCurrency(price)}</span>
+        </Card.Title>
+        <div className="mt-auto">
+          <Button variant="primary">Add to Cart</Button>
+        </div>
+      </Card.Body>
     </Card>
   );
 }
-
 export default StoreItem;
