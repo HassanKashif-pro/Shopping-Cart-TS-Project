000137e9985b90b498a582c94b409bca71bb048c
Hassan Kashif. pro <hassan.kashif.pro@Gmail.com>
2024-05-27 14:17:52 -0700

    Added The "Add to cart" button, + and - button and the remove button.

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -9,6 +9,7 @@ type StoreItemProps = {
 };
 
 export function StoreItem({ id, imgUrl, name, price }: StoreItemProps) {
+  const quantity = 0;
   return (
     <Card className="h-100">
       <Card.Img
@@ -23,7 +24,28 @@ export function StoreItem({ id, imgUrl, name, price }: StoreItemProps) {
           <span className="ms-2 text-muted">{formatCurrency(price)}</span>
         </Card.Title>
         <div className="mt-auto">
-          <Button variant="primary">Add to Cart</Button>
+          {quantity === 0 ? (
+            <Button className="w-100">+ Add To Cart</Button>
+          ) : (
+            <div
+              className="d-flex align-items-center flex-column"
+              style={{ gap: ".5rem" }}
+            >
+              <div
+                className="d-flex align-items-center justify-content-center"
+                style={{ gap: ".5rem" }}
+              >
+                <Button>-</Button>
+                <div>
+                  <span className="fs-3">{quantity}</span> in cart
+                </div>
+                <Button>+</Button>
+              </div>
+              <Button variant="danger" size="sm">
+                Remove
+              </Button>
+            </div>
+          )}
         </div>
       </Card.Body>
     </Card>
