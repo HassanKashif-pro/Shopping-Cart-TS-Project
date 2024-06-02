c5fb4a33a987b9558aada2eacaa233e1bb236c64
Hassan Kashif. pro <hassan.kashif.pro@Gmail.com>
2024-06-02 16:17:32 -0700

    Added the increase cart Item quantity functionality.

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,11 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Navbar from "./components/NavBar";
 import { Store } from "./pages/Store";
+import { ShoppingCartProviderProps } from "./context/ShoppingCartContext";
 
 function App() {
   return (
-    <>
+    <ShoppingCartProviderProps>
       <Navbar />
       <Container className="mb-4">
         <Routes>
@@ -16,7 +17,7 @@ function App() {
           <Route path="/about" element={<About />} />
         </Routes>
       </Container>
-    </>
+    </ShoppingCartProviderProps>
   );
 }
 
