92f31e9f8ed3d16a68f795abfe941aa0e53593cd
Hassan Kashif. pro <hassan.kashif.pro@Gmail.com>
2024-06-02 19:23:10 -0700

    Added the decrease Cart Quantity functionality.

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -46,9 +46,24 @@ export function ShoppingCartProviderProps({
     });
   }
 
+  function decreaseCartQuantity(id: number) {
+    setCartItems((currItems) => {
+      if (currItems.find((item) => item.id === id)?.quantity === 1) {
+        return currItems.filter((item) => item.id !== id);
+      } else {
+        return currItems.map((item) => {
+          if (item.id === id) {
+            return { ...item, quantity: item.quantity - 1 };
+          } else {
+            return item;
+          }
+        });
+      }
+    });
+  }
   return (
     <ShoppingCartContext.Provider
-      value={{ getItemQuantity, increaseCartQuantity }}
+      value={{ getItemQuantity, increaseCartQuantity, decreaseCartQuantity }}
     >
       {children}
     </ShoppingCartContext.Provider>
